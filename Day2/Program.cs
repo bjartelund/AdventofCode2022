e19a70566e05d4eb389cb102dcc24922a077f19d
Bjarte Aarmo Lund <bjarte@godkjemi.net>
2022-12-02 23:24:11 +0100

    Fix up the second part and make the code compile without warnings

--- Day2/Program.cs
+++ Day2/Program.cs
@@ -1,9 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-
-using System.Runtime.CompilerServices;
-
-Console.WriteLine("Hello, World!");
-var lines = File.ReadAllLines("input");
+﻿var lines = File.ReadAllLines("input");
 int score = 0;
 foreach (var line in lines)
 {
@@ -13,7 +8,16 @@ foreach (var line in lines)
         "X" => CalculateWinner(plays[0], plays[1])+ 1,
         "Y" => CalculateWinner(plays[0], plays[1]) + 2,
         "Z" => CalculateWinner(plays[0], plays[1]) + 3,
+        _ => 0
     };
+    score += roundscore;
+}
+Console.WriteLine(score);
+score=0;
+foreach (var line in lines)
+{
+    var plays = line.Split(' ');
+    var roundscore = CalculateWinner2(plays[0], GetPlayForOutcome(plays[0],plays[1]));
     score += roundscore;
 }
 Console.WriteLine(score);
@@ -47,16 +51,16 @@ int CalculateWinner(string player1, string player2)
         }
         case "C":
         {
-            if (player2.Equals("Z"))
-            {
-                score = 3;
-            }
-
-            if (player2.Equals("X"))
-            {
-                score = 6;
-            }
-
+            score = player2 switch {
+                "Z" => 3,
+                "X" => 6,
+                _ => score,
+            };
+            
+            break;
+        }
+        default:
+        {
             break;
         }
     }
@@ -64,12 +68,82 @@ int CalculateWinner(string player1, string player2)
     return score;
 }
 
-int CalculateScoreForOutcome(string play, string outcome)
+int CalculateWinner2(string player1, string player2)
 {
+    var score = 0;
+    switch (player1)
+    {
+        case "A":
+        {
+            score = player2 switch
+            {
+                "X" => 3+1,
+                "Y" => 6+2,
+                "Z" => 0+3,
+                _ => score
+            };
+
+            break;
+        }
+        case "B":
+        {
+            score = player2 switch
+            {
+                "Y" => 3+2,
+                "Z" => 6+3,
+                "X" => 0+1,
+                _ => score
+            };
+
+            break;
+        }
+        case "C":
+        {
+            score = player2 switch{
+                "Z" => 3+3,
+                "X" => 6+1,
+                "Y" => 0+2,
+                _ => score
+            };
+
+            break;
+        }
+        default:
+        {
+            break;
+        }
+    }
+
+    return score;
+}
+
+string GetPlayForOutcome(string play, string outcome)
+{
+
+    //x lose
+    //y draw
+    //z win
+    //a - x rock
+    //b - y paper
+    //c - z scissor 
     if (play.Equals("A"))
     {
-        if (outcome.Equals("X")) return 0;
-        if (outcome.Equals("Y")) return 2;
-        if (outcome.Equals("Z")) 
+        if (outcome.Equals("X")) return "Z";
+        if (outcome.Equals("Y")) return "X";
+        if (outcome.Equals("Z")) return "Y";
     }
+        if (play.Equals("B"))
+    {
+        if (outcome.Equals("X")) return "X";
+        if (outcome.Equals("Y")) return "Y";
+        if (outcome.Equals("Z")) return "Z";
+    }
+            if (play.Equals("C"))
+    {
+        if (outcome.Equals("X")) return "Y";
+        if (outcome.Equals("Y")) return "Z";
+        if (outcome.Equals("Z")) return "X";
+    }
+    return "";
 }
+
